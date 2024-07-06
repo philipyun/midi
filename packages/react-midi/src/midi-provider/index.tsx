8641f3b24cde53b856c5ead65c04ecdb40ff431c
Philip Yun <philip.yun103@gmail.com>
2024-07-06 04:30:41 -0700

    more config stuff for better intellisense

---
+++ packages/react-midi/src/midi-provider/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { AvailableMIDIPorts, requestMIDIAccess } from '@philipyun-midi/midi/protocol/permissions';
+
+interface MIDIContextValue {
+  midiPorts: AvailableMIDIPorts | null;
+}
+
+interface MIDIContextProps {
+  children: ReactNode;
+}
+
+const MIDIContext = createContext({} as MIDIContextValue);
+
+export const useMIDI = () => {
+  return useContext(MIDIContext);
+};
+
+export const MIDIProvider: React.FC<MIDIContextProps> = ({ children }) => {
+  const [midiPorts, setMidiPorts] = useState<AvailableMIDIPorts | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const access = await requestMIDIAccess();
+      setMidiPorts(access);
+    };
+    init();
+    requestMIDIAccess();
+  }, []);
+
+  const contextValue: MIDIContextValue = {
+    midiPorts,
+  };
+
+  return <MIDIContext.Provider value={contextValue}>{children}</MIDIContext.Provider>;
+};
