86e111f140fe09bd74ab937660a39deeaf7387b2
Philip Yun <philip.yun103@gmail.com>
2024-07-06 07:40:13 -0700

    link inputs and outputs together

--- packages/midi/src/protocol/permissions.ts
+++ packages/midi/src/protocol/permissions.ts
@@ -1,12 +1,29 @@
+export interface LinkedMIDIPort {
+  name: string;
+  manufacturer: string;
+  input: MIDIInput;
+  output: MIDIOutput;
+}
+
+export type AvailableMIDIPorts = LinkedMIDIPort[];
+
 export const requestMIDIAccess = async () => {
   const access = await navigator.requestMIDIAccess();
   const inputs = Array.from(access.inputs.values());
   const outputs = Array.from(access.outputs.values());
 
-  return {
-    inputs,
-    outputs,
-  };
-};
+  return outputs.reduce((acc, output) => {
+    const { name, manufacturer } = output;
+    const input = inputs.find(i => i.name === output.name && i.manufacturer === output.manufacturer);
 
-export type AvailableMIDIPorts =  Awaited<ReturnType<typeof requestMIDIAccess>>;
+    if (input && name !== null && manufacturer !== null) {
+      acc.push({
+        name,
+        manufacturer,
+        input,
+        output,
+      });
+    }
+    return acc;
+  }, [] as AvailableMIDIPorts);
+};
